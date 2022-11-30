0487492590f750b5d394530f33fcd6989a2a3195
1Tp <1Tp@207-2>
2022-11-30 12:19:44 +0100

    Dodaj pliki projektów.

---
+++ FirstGitHubProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstGitHubProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Connect to GitHub : Widok -> Zmiany GIT");
+        }
+    }
+}
